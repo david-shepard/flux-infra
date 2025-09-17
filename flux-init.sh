7076a2b21e38ac6444fbd2b1cd1a50f60dd5c531
David Shepard <davidshepard77@gmail.com>
2025-09-17 16:18:47 -0400

    feat: add podinfo and refactor

---
+++ flux-init.sh
@@ -0,0 +1,6 @@
+flux bootstrap github \                                          ✔  5s   03:55:13 PM 
+  --owner=$GITHUB_USER \
+  --repository=flux-infra \
+  --branch=main \
+  --path=./clusters/dev \
+  --personal
